5e8de296426692c802abda2442b8e2b0fdb687cf
SergeyGuns <mcheguevara2@gmail.com>
2017-05-12 19:57:27 +0300

    fix

--- src/components/Tetris/GameLoop.js
+++ src/components/Tetris/GameLoop.js
@@ -1,7 +1,25 @@
 export default function GameLoop (matrix) {
     console.log('GameLoop Step')
-    function freezing(num) {
+    let toDrop = false;
+    let toFreez = 0;
 
+    function check() {
+      for(let Y = matrix.length-1; Y >= 0; Y--) {
+        for(let X = matrix[Y].length-1; X >= 0; X--) {
+
+          if( matrix[Y+1] && matrix[Y+1][X] === 0 && matrix[Y][X] !== 9) {
+            toDrop = true;
+          }
+          if ( matrix[Y+1] && matrix[Y+1][X] === 9 && matrix[Y][X] !== 0 ) {
+            toFreez = matrix[Y][X];
+          }
+        }
+      }
+      toFreez && freezing(toFreez)
+      toDrop && dropin()
+    }
+
+    function freezing(num) {
       for(let Y = matrix.length-1; Y >= 0; Y--) {
         for(let X = matrix[Y].length-1; X >= 0; X--) {
           if(matrix[Y][X] === num) {
@@ -9,21 +27,24 @@ export default function GameLoop (matrix) {
           }
         }
       }
-
+      toFreez = 0;
     }
-    for(let Y = matrix.length-1; Y >= 0; Y--) {
-      for(let X = matrix[Y].length-1; X >= 0; X--) {
 
-        if( matrix[Y+1] && matrix[Y+1][X] === 0 && matrix[Y][X] !== 9) {
-          matrix[Y+1][X] = matrix[Y][X]
-          matrix[Y][X] = 0
-        }
-        if ( matrix[Y+1] && matrix[Y+1][X] === 9 && matrix[Y][X] !== 0 ) {
-          freezing(matrix[Y][X])
-        }
+    function dropin() {
 
+      for(let Y = matrix.length-1; Y >= 0; Y--) {
+        for(let X = matrix[Y].length-1; X >= 0; X--) {
+
+          if( matrix[Y+1] && matrix[Y+1][X] === 0 && matrix[Y][X] !== 9) {
+            matrix[Y+1][X] = matrix[Y][X]
+            matrix[Y][X] = 0
+          }
+        }
       }
+      toDrop = false;
     }
+    check()
+
 
 
 
